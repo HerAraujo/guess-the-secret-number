55c2118dfbcfc260f2b43e51bb1fce36af60e636
Hernan Araujo <hernan.araujo@cencosud.com.uy>
2023-07-03 10:30:44 -0300

    DRY Principle, refactoring code

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
-
+let secretNumber;
 let count = 20;
 let highscore = localStorage.getItem('highscore');
 document.querySelector('.highscore').textContent = highscore;
+
+const assignSecretNumber = () => {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+};
+assignSecretNumber();
+
+const changeMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const changeBackground = color => {
+  document.body.style.backgroundColor = color;
+};
+
+const changeScore = score => {
+  document.querySelector('.score').textContent = score;
+};
+
+const changeHighscore = highscore => {
+  document.querySelector('.highscore').textContent = highscore;
+};
+
+const changeSecretNumber = value => {
+  document.querySelector('.number').textContent = value;
+};
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = document.querySelector('.guess').value;
   /* SI ACIERTA EL NUMERO SECRETO - TODO: REINICIAR MARCADOR LUEGO DE ACERTAR Y DESHABILITAR EL BOTON DE CHECK */
   if (secretNumber === +guess) {
-    document.querySelector('.message').textContent = '🎉 Correct!'; // MENSAJE CAMBIA A "CORRECT"
-    document.querySelector('.number').textContent = secretNumber; // REVELA NUMERO SECRETO
-    document.body.style.backgroundColor = 'green'; // BACKGROUND A GREEN
+    changeMessage('🎉 Correct!'); // MENSAJE CAMBIA A "CORRECT"
+    changeSecretNumber(secretNumber); // REVELA NUMERO SECRETO
+    changeBackground('green'); // BACKGROUND A GREEN
     if (count > highscore) {
       highscore = count;
-      document.querySelector('.highscore').textContent = highscore; // ACTUALIZA HIGHSCORE SI ES NECESARIO
+      changeHighscore(highscore); // ACTUALIZA HIGHSCORE SI ES NECESARIO
     }
   } else if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number'; // SI NO ENVIAN NUMERO, MENSAJE DE ERROR
+    changeMessage('⛔️ No number'); // SI NO ENVIAN NUMERO, MENSAJE DE ERROR
   } else if (count <= 1) {
-    document.querySelector('.message').textContent = '💥 YOU LOST'; // SI EL CONTADOR LLEGA A 0, MENSAJE DE PERDISTE
-    document.querySelector('.number').textContent = secretNumber; // REVELA NUMERO SECRETO
+    changeMessage('💥 YOU LOST'); // SI EL CONTADOR LLEGA A 0, MENSAJE DE PERDISTE
+    changeSecretNumber(secretNumber); // REVELA NUMERO SECRETO
     count = 0;
-    document.body.style.backgroundColor = 'red'; // BACKGROUND A RED
-    document.querySelector('.score').textContent = count;
+    changeBackground('red'); // BACKGROUND A RED
+    changeScore(count);
   } else {
     // CASO DE QUE SE EQUIVOQUE
     count--; // RESTA UN PUNTO
-    document.querySelector('.score').textContent = count;
-    document.querySelector('.message').textContent = '❌ Try again!'; // MUESTRA MENSAJE DE INTENTAR DE NUEVO
+    changeScore(count);
+    changeMessage('❌ Try again!'); // MUESTRA MENSAJE DE INTENTAR DE NUEVO
   }
   localStorage.setItem('highscore', highscore);
 });
 
 document.querySelector('.again').addEventListener('click', () => {
-  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  assignSecretNumber();
   count = 20;
   document.querySelector('.guess').value = null;
-  document.querySelector('.number').textContent = '?'; // REVELA NUMERO SECRETO
-  document.querySelector('.score').textContent = count;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  changeSecretNumber('?'); // REVELA NUMERO SECRETO
+  changeScore(count);
+  changeBackground('#222');
+  changeMessage('Start guessing...');
 });
